5511dce8e2e619c7552f4f91dc8bbe15f83ffe58
Nathan Santos Ataliba <nathan.s@inatel.br>
2023-09-21 14:31:12 -0300

    Atualizacoes

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # C214_TRABALHO
 
-## Logica utlizada para o código:
+## Logica utilizada para o código:
 
+### Nomes:
+- Não será permitido nomes iguais.
+
+### Periodo:
+- É permitido o periodo Integral ou Noturno.
+
+### Salas e predios:
 - Sala(1 a 5) -> predio 1
 - Sala(6 a 10) -> predio 2
 - Sala(11 a 15) -> predio 3
 - Sala(16 a 20) -> predio 4
 - Sala(21 a 25) -> predio 5
+
 
 
--- test/frontTests.js
+++ test/frontTests.js
@@ -43,12 +43,12 @@ describe('Testes no frontend:',() => {
         const respostaVerificacao = verificaPredio(sala);
         expect(respostaVerificacao).to.equal(3);
     })
-    it('Teste numero do predio 3: ',() => {
+    it('Teste numero do predio 4: ',() => {
         const sala = 20; 
         const respostaVerificacao = verificaPredio(sala);
         expect(respostaVerificacao).to.equal(4);
     })
-    it('Teste numero do predio 4: ',() => {
+    it('Teste numero do predio 5: ',() => {
         const sala = 25; 
         const respostaVerificacao = verificaPredio(sala);
         expect(respostaVerificacao).to.equal(5);
